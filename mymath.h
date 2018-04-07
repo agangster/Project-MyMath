14f92c7088c572d23133ac5cd8ec32d6a9433821
agangster <38050533+agangster@users.noreply.github.com>
2018-04-07 19:50:25 +0200

    Add files via upload

---
+++ mymath.h
@@ -0,0 +1,16 @@
+double addX(double x1, double x2); //adding coordinates
+
+double substractX(double x1, double x2); //substracting coordinates
+
+double scalarProduct(double x1, double y1, double x2, double y2);
+
+double sqrt( int n );
+
+double lengthOfvector(double x, double y); // coordinates of endpoint
+
+double lengthOfsegment(double xA, double yA,  double xB, double yB); // coordinates of endpoints
+
+
+double midpointsXvalue(double xA, double xB);
+
+double midpointsYvalue(double yA, double yB);
